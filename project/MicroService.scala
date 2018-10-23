14f8a54ba48105b57fa09f0c915d6f1a040a9b8a
Richard Dennehy <richard.dennehy@digital.hmrc.gov.uk>
2018-10-23 11:45:59 +0100

    migrate to new build

--- project/MicroService.scala
+++ project/MicroService.scala
@@ -2,8 +2,9 @@ import sbt.Keys._
 import sbt.Tests.{SubProcess, Group}
 import sbt._
 import play.routes.compiler.StaticRoutesGenerator
+import uk.gov.hmrc.SbtArtifactory
 import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin._
-
+import uk.gov.hmrc.versioning.SbtGitVersioning.autoImport.majorVersion
 
 trait MicroService {
 
@@ -14,22 +15,21 @@ trait MicroService {
   import uk.gov.hmrc.versioning.SbtGitVersioning
   import play.sbt.routes.RoutesKeys.routesGenerator
 
-
-  import TestPhases._
-
   val appName: String
 
-  lazy val appDependencies : Seq[ModuleID] = ???
-  lazy val plugins : Seq[Plugins] = Seq.empty
-  lazy val playSettings : Seq[Setting[_]] = Seq.empty
+  lazy val appDependencies: Seq[ModuleID] = ???
+  lazy val plugins: Seq[Plugins] = Seq.empty
+  lazy val playSettings: Seq[Setting[_]] = Seq.empty
 
   def intTestFilter(name: String): Boolean = name startsWith "it"
+
   def unitFilter(name: String): Boolean = name startsWith "unit"
+
   def componentFilter(name: String): Boolean = name startsWith "component"
 
   lazy val microservice = Project(appName, file("."))
-    .enablePlugins(Seq(play.sbt.PlayScala,SbtAutoBuildPlugin, SbtGitVersioning, SbtDistributablesPlugin) ++ plugins : _*)
-    .settings(playSettings : _*)
+    .enablePlugins(Seq(play.sbt.PlayScala, SbtAutoBuildPlugin, SbtGitVersioning, SbtDistributablesPlugin, SbtArtifactory) ++ plugins: _*)
+    .settings(playSettings: _*)
     .settings(scalaSettings: _*)
     .settings(publishingSettings: _*)
     .settings(defaultSettings(): _*)
@@ -38,14 +38,14 @@ trait MicroService {
       testOptions in Test := Seq(Tests.Filter(unitFilter)),
       retrieveManaged := true,
       evictionWarningOptions in update := EvictionWarningOptions.default.withWarnScalaVersionEviction(false),
-      unmanagedResourceDirectories in Test <<= (baseDirectory in Test)(base => Seq(base / "test/resources")),
+      unmanagedResourceDirectories in Test <<= (baseDirectory in Test) (base => Seq(base / "test/resources")),
       routesGenerator := StaticRoutesGenerator
     )
     .configs(IntegrationTest)
     .settings(inConfig(IntegrationTest)(Defaults.itSettings): _*)
     .settings(
       Keys.fork in IntegrationTest := false,
-      unmanagedSourceDirectories in IntegrationTest <<= (baseDirectory in IntegrationTest)(base => Seq(base / "test")),
+      unmanagedSourceDirectories in IntegrationTest <<= (baseDirectory in IntegrationTest) (base => Seq(base / "test")),
       testOptions in IntegrationTest := Seq(Tests.Filter(intTestFilter)),
       addTestReportOption(IntegrationTest, "int-test-reports"),
       testGrouping in IntegrationTest := oneForkedJvmPerTest((definedTests in IntegrationTest).value),
@@ -55,21 +55,19 @@ trait MicroService {
     .settings(
       Keys.fork in ComponentTest := false,
       testOptions in ComponentTest := Seq(Tests.Filter(componentFilter)),
-      unmanagedSourceDirectories   in ComponentTest <<= (baseDirectory in ComponentTest)(base => Seq(base / "test")),
+      unmanagedSourceDirectories in ComponentTest <<= (baseDirectory in ComponentTest) (base => Seq(base / "test")),
       testGrouping in ComponentTest := oneForkedJvmPerTest((definedTests in ComponentTest).value),
       parallelExecution in ComponentTest := false
     )
-      .settings(resolvers ++= Seq(
-        Resolver.bintrayRepo("hmrc", "releases"),
-        Resolver.jcenterRepo
-      ))
-}
-
-private object TestPhases {
+    .settings(resolvers ++= Seq(
+      Resolver.bintrayRepo("hmrc", "releases"),
+      Resolver.jcenterRepo
+    ))
+    .settings(majorVersion := 0)
 
   lazy val ComponentTest = config("component") extend Test
 
-  def oneForkedJvmPerTest(tests: Seq[TestDefinition]) =
+  private def oneForkedJvmPerTest(tests: Seq[TestDefinition]) =
     tests map {
       test => Group(test.name, Seq(test), SubProcess(ForkOptions(runJVMOptions = Seq("-Dtest.name=" + test.name))))
     }

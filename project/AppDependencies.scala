8296f953803154fb0b932b648a95d3b569c60dce
Łukasz Wrona <lukasz.andrzej.wrona@outlook.com>
2024-03-15 16:34:23 +0000

    DLS-10101 Update to Play 3.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,18 +2,18 @@ import sbt._
 
 object AppDependencies {
 
-  val hmrcBootstrapVersion = "7.23.0"
+  val hmrcBootstrapVersion = "8.4.0"
+  val playVersion = "play-30"
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % hmrcBootstrapVersion,
-    "uk.gov.hmrc" %% "domain" % "8.0.0-play-28",
-    "com.typesafe.play" %% "play-json-joda" % "2.9.1"
+    "uk.gov.hmrc" %% s"bootstrap-backend-$playVersion" % hmrcBootstrapVersion,
+    "uk.gov.hmrc" %% s"domain-$playVersion"            % "9.0.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-28" % hmrcBootstrapVersion % "test, it",
-    "org.scalaj" %% "scalaj-http" % "2.4.2" % "test, it",
-    "com.github.tomakehurst" % "wiremock-jre8" % "2.27.2" % "test, it",
-    "org.mockito" % "mockito-scala_2.13" % "1.17.12" % "test, it",
+    "uk.gov.hmrc"           %% s"bootstrap-test-$playVersion" % hmrcBootstrapVersion % "test, it",
+    "org.scalaj"            %% "scalaj-http"                  % "2.4.2"              % "test, it",
+    "org.mockito"            % "mockito-scala_2.13"           % "1.17.30"            % "test, it"
+
   )
 }
